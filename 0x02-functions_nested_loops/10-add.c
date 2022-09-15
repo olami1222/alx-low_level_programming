fe55cbecc7a2ebc6aed639bc882bbe9bae9d1cb0
olami1222 <wazirolamide22@gmail.com>
2022-09-15 03:33:24 -0700

    completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Add two numbers
+ * @a: integer type number
+ * @b: integer type number
+ * Return: result of sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *
+ * main - main body
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
