46d4fd2ed20a2418379e902bc6860919f089d5b3
olami1222 <wazirolamide22@gmail.com>
2022-10-06 19:24:53 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  malloc_checked - function that allocates memory
+ *  @b: number of memory to be allocated
+ *  Return: Void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *spike;
+	spike = malloc(b);
+
+	if (spike == NULL)
+		exit(98);
+
+}
